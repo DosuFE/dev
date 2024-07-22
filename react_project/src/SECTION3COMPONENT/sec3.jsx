ce5bc2c018fa3a69536f845916007fcc5a6e451e
DosuFE <sdosu@dervac.com>
2024-07-22 19:47:11 +0100

    Addition of sliding image

--- react_project/src/SECTION3COMPONENT/sec3.jsx
+++ react_project/src/SECTION3COMPONENT/sec3.jsx
@@ -5,15 +5,15 @@ const Sec3 = () => {
     <>  
       <section>
         <main>
-            <div className='font-lato text-center '>
-                <h1 className='text-lg font-semibold tracking-wider md:text-3xl md:uppercase'>
-                    EXCLUSIVE LISTINGS
-                </h1>
-                <p className='text-[#808080] pt-2 px-5 text-sm leading-6 font-medium md:text-base
+            <div className='font-lato text-center pt-8 md:pt-10'>
+              <h1 className='text-lg font-semibold tracking-wider md:text-3xl md:uppercase'>
+                EXCLUSIVE LISTINGS
+              </h1>
+              <p className='text-[#808080] pt-2 px-5 text-sm leading-6 font-medium md:text-base
                 md:tracking-wider'>
-                    Mauris primis turpis Laoreet magna felis mi amet quam enim curae. Sodales semper 
-                    tempor dictum faucibus habitasse.
-                </p>
+                Mauris primis turpis Laoreet magna felis mi amet quam enim curae. Sodales semper 
+                tempor dictum faucibus habitasse.
+              </p>
             </div>
         </main>
       </section>
@@ -21,4 +21,4 @@ const Sec3 = () => {
   )
 }
 
-export default  Sec3
+export default  Sec3;
